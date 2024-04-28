488ee30ffc5fa3d0f74020ad8d72c1c9169a8402
Dennis <dennisfelix131@gmail.com>
2024-04-28 17:34:33 +0100

    Removed some files

--- index.js
+++ index.js
@@ -454,7 +454,7 @@ const updateBook = function (req, res, bookToUpdate) {
 // Create server
 const server = http.createServer(requestHandler)
 
-server.listen(PORT, HOST_NAME, () => {
+server.listen(port, HOST_NAME, () => {
     booksDB = JSON.parse(fs.readFileSync(booksDbPath, 'utf8'));
     usersDB = JSON.parse(fs.readFileSync(usersDbPath, 'utf8'));
     console.log(`Server is listening on ${HOST_NAME}:${port}`)
